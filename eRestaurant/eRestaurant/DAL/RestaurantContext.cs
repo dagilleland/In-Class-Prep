bc1baa1c6ddab30c1b3d6d87d8fb42a8351279ce
Dan Gilleland <dgilleland@nait.ca>
2014-09-10 12:40:23 -0600

    RestaurantContext:DbContext

---
+++ eRestaurant/eRestaurant/DAL/RestaurantContext.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using eRestaurant.Entities;
+
+namespace eRestaurant.DAL
+{
+    internal class RestaurantContext : DbContext
+    {
+        public RestaurantContext() : base("name=EatIn") { }
+
+        public DbSet<Table> Tables { get; set; }
+        public DbSet<SpecialEvent> SpecialEvents { get; set; }
+        public DbSet<Reservation> Reservations { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .Entity<Reservation>().HasMany(r => r.Tables)
+                .WithMany(t => t.Reservations)
+                .Map(mapping =>
+                {
+                    mapping.ToTable("ReservationTables");
+                    mapping.MapLeftKey("ReservationID");
+                    mapping.MapRightKey("TableID");
+                });
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
