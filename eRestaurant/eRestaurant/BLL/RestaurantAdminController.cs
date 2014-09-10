5aec16c9426eedddd3051d4601c62ed4ae6a10cd
Dan Gilleland <dgilleland@nait.ca>
2014-09-10 12:51:53 -0600

    BLL - RestaurantAdminController

---
+++ eRestaurant/eRestaurant/BLL/RestaurantAdminController.cs
@@ -0,0 +1,23 @@
+﻿using eRestaurant.DAL;
+using eRestaurant.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eRestaurant.BLL
+{
+    public class RestaurantAdminController
+    {
+        #region Manage Tables
+        public List<Table> ListAllTables()
+        {
+            using (RestaurantContext context = new RestaurantContext())
+            {
+                return context.Tables.ToList();
+            }
+        }
+        #endregion
+    }
+}
