e29fd1aeefcb5066edee1885819ee2ca0a4c8009
dagilleland <dagilleland@shaw.ca>
2014-11-08 04:25:27 -0700

    Security: ASP.NET Identity + backfill Entities

---
+++ eRestaurant/eRestaurant/Entities/MenuCategory.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+
+namespace eRestaurant.Entities
+{
+    public class MenuCategory
+    {
+        public int MenuCategoryID { get; set; }
+        [Required(ErrorMessage = "A Description is required (5-35 characters)")]
+        [StringLength(35, MinimumLength = 5, ErrorMessage = "Descriptions must be from 5 to 35 characters in length")]
+        public string Description { get; set; }
+
+        public virtual ICollection<Item> MenuItems { get; set; }
+    }
+}
